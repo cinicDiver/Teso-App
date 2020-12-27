9a9af5e45b4d720746596961faadf2102a19b600
cinicDiver <oscarframirezp@hotmail.com>
2020-12-26 19:19:31 -0500

    Minor changes.

--- login.js
+++ login.js
@@ -7,20 +7,20 @@ var firebaseConfig = {
     appId: "1:1099449504304:web:7a9d46e4a8ec16bac693f5",
     measurementId: "G-NZDSGZXQ66"
   };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 
-  const uName = document.getElementById("uName");
-  const uPass = document.getElementById("pwLine");
-  const inBtn = document.getElementById("btnEntrar");
+const uName = document.getElementById("uName");
+const uPass = document.getElementById("pwLine");
+const inBtn = document.getElementById("btnEntrar");
 
-  inBtn.addEventListener("click", e =>{
-      const userIn = uName.value;
-      const userPw = uPass.value;
-      const auth = firebase.auth();
-      console.log("Ingresando al usuario: "+userIn);
-      const promise = auth.signinwithemailandpassword(userIn,userPw);
-      promise.catch(e => console.log(e.message()));
-  });
+inBtn.addEventListener("click", e =>{
+    const userIn = uName.value;
+    const userPw = uPass.value;
+    const auth = firebase.auth();
+    console.log("Ingresando al usuario: "+userIn);
+    const promise = auth.signInWithEmailAndPassword(userIn,userPw);
+    promise.catch(e => console.log(e.message()));
+});
 
